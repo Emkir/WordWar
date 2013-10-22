8e14507ed9f4a16737165eaf2fbf6a8025cf7ca5
Emmeric Royer <emmeric.royer@supinternet.fr>
2013-10-22 16:21:58 +0200

    init js

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(function() {
+    var healthPoints;
+    var enemyHealthPoints;
+
+    var timeRemain;
+    var enemyTimeRemain;
+
+    var rocketDamages;
+    var enemyRocketDamages;
+
+    var word;
+
+    function initHP(hp){
+        hp = 1000;
+    }
+
+    function initDamages(damages){
+        damages = 500;
+    }
+
+    function initTime(timeRemain){
+        timeRemain = 30;
+    }
+
+    function newRocket(player){
+        
+    }
+
+    function newParty(){
+        initHP(healthPoints);
+        initHP(enemyHealthPoints);
+
+    }
+
+});
